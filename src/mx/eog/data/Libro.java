45a14b6f0647e690b2994f0a027b8220d5bce7a8
emmanuel <eoropezag3@gmail.com>
2020-02-28 20:43:27 -0600

    initial commit

---
+++ src/mx/eog/data/Libro.java
@@ -0,0 +1,17 @@
+package mx.eog.data;
+
+public class Libro {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j = 0;
+		for(int i = 0; i < 5; ++i)
+		{
+			i = ++j;
+		}
+		System.out.println("i=" + j);
+	}
+	
+	
+}
